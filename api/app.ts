df094653961df0023749c265d2a03c429912c02e
Ben Walker <bwalke05@gmail.com>
2020-09-12 11:33:32 -0400

    Move graphql schema back to root

--- api/app.ts
+++ api/app.ts
@@ -1,11 +1,5 @@
-import { settings, use } from "nexus";
+import { use } from "nexus";
 import { prisma } from "nexus-plugin-prisma";
-
-settings.change({
-  schema: {
-    generateGraphQLSDLFile: "api/schema.graphql",
-  },
-});
 
 use(
   prisma({
