db025dbf85ffa512ad837f25ef196a01b26a33f9
Ben Walker <bwalke05@gmail.com>
2020-09-20 15:29:16 -0400

    Add seeding info print

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -4,6 +4,7 @@ import { seedUser } from "./models/user";
 const seed = async () => {
   const prisma = new PrismaClient();
 
+  console.info("Seeding database...");
   await seedUser(prisma);
 
   prisma.$disconnect();
