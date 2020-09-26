58642191280f23d24e6075feec782d4f937fcc2e
Ben Walker <bwalke05@gmail.com>
2020-09-26 11:10:29 -0400

    Add auth payload and mock registration resolver

--- api/schema/User.ts
+++ api/schema/User.ts
@@ -1,3 +1,4 @@
+import { hash, argon2id } from "argon2";
 import { schema } from "nexus";
 
 schema.objectType({
@@ -8,10 +9,35 @@ schema.objectType({
   },
 });
 
+schema.objectType({
+  name: "AuthPayload",
+  definition(t) {
+    t.string("token");
+    t.field("user", { type: "User" });
+  },
+});
+
 schema.extendType({
   type: "Query",
   definition(t) {
     t.crud.user();
     t.crud.users();
+  },
+});
+
+schema.extendType({
+  type: "Mutation",
+  definition(t) {
+    t.field("register", {
+      type: "AuthPayload",
+      args: {
+        email: schema.stringArg({ required: true }),
+        password: schema.stringArg({ required: true }),
+      },
+      resolve: async (_root, { email, password }, ctx) => {
+        const hashedPassword = await hash(password, { type: argon2id });
+        return null;
+      },
+    });
   },
 });
