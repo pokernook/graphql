d1f0236fc2209fa79bff41c9250b39f5f707382c
Ben Walker <bwalke05@gmail.com>
2020-09-30 19:25:27 -0400

    Migrate use of prisma plugin

--- api/schema.ts
+++ api/schema.ts
@@ -1,10 +1,29 @@
+import { DateTimeResolver, JSONObjectResolver } from "graphql-scalars";
 import { makeSchema } from "@nexus/schema";
+import { nexusPrisma } from "nexus-plugin-prisma";
 import { join } from "path";
 
 export const schema = makeSchema({
-  types: [],
   outputs: {
     schema: join(__dirname, "api.graphql"),
     typegen: join(__dirname, "nexus-typegen.ts"),
   },
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+      paginationStrategy: "prisma",
+      scalars: {
+        DateTime: DateTimeResolver,
+        Json: JSONObjectResolver,
+      },
+    }),
+  ],
+  typegenAutoConfig: {
+    sources: [
+      { alias: "prisma", source: require.resolve(".prisma/client/index.d.ts") },
+      { alias: "ContextModule", source: require.resolve("./context") },
+    ],
+    contextType: "ContextModule.Context",
+  },
+  types: [],
 });
