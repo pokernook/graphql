dafcd1b9274899871cf305f3c9a1734b4abba0f2
Ben Walker <bwalke05@gmail.com>
2020-09-30 22:57:41 -0400

    Consolidate server setup within app

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,13 @@
+import { ApolloServer } from "apollo-server-express";
+import createExpress from "express";
 import { config } from "./config";
-import { server } from "./server";
+import { schema } from "./schema";
 
-server.listen(config.port, () => {
+const apollo = new ApolloServer({ schema });
+const app = createExpress();
+
+apollo.applyMiddleware({ app });
+
+app.listen(config.port, () => {
   console.log(`🚀 Server ready at http://localhost:${config.port}`);
 });
--- api/server.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloServer } from "apollo-server-express";
-import createExpress from "express";
-import { schema } from "./schema";
-
-const apollo = new ApolloServer({ schema });
-export const server = createExpress();
-
-apollo.applyMiddleware({ app: server });
