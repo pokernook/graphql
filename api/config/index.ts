bd16d8505a6ca11a723f0f67e5d8cff495e7531d
Ben Walker <bwalke05@gmail.com>
2020-10-03 19:51:15 -0400

    Provide default for app secret, remove required status

--- api/config/index.ts
+++ api/config/index.ts
@@ -1,6 +1,6 @@
 import env from "env-var";
 
 export const config = {
-  appSecret: env.get("APP_SECRET").required().asString(),
+  appSecret: env.get("APP_SECRET").default("").asString(),
   port: env.get("PORT").default(3000).asPortNumber(),
 };
