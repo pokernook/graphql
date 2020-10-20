94cc70c9bb540df99a5d7787700564f600362719
Ben Walker <bwalke05@gmail.com>
2020-10-20 19:09:48 -0400

    Remove isOffline env variable

--- api/server/schema.ts
+++ api/server/schema.ts
@@ -2,18 +2,15 @@ import { makeSchema } from "@nexus/schema";
 import { nexusPrisma } from "nexus-plugin-prisma";
 import { join } from "path";
 
-import { config } from "../config";
 import * as types from "../graphql";
 
 export const schema = makeSchema({
-  shouldGenerateArtifacts: config.isOffline,
   outputs: {
     schema: join(__dirname, "../api.graphql"),
     typegen: join(__dirname, "../node_modules/@types/nexus-typegen/index.d.ts"),
   },
   plugins: [
     nexusPrisma({
-      shouldGenerateArtifacts: config.isOffline,
       experimentalCRUD: true,
       paginationStrategy: "prisma",
       outputs: {
