d844952d9cdf902d7ea4d142f6ec92d1677f50da
Ben Walker <bwalke05@gmail.com>
2020-10-25 15:50:45 -0400

    Add NODE_ENV to configuration

--- api/config/index.ts
+++ api/config/index.ts
@@ -2,5 +2,6 @@ import env from "env-var";
 
 export const config = {
   appSecret: env.get("APP_SECRET").default("").asString(),
+  env: env.get("NODE_ENV").default("development").asString(),
   port: env.get("PORT").default(3000).asPortNumber(),
 };
