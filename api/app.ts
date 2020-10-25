b931f4d1e24ffa4e0108fdbb03b4a2c43e24867a
Ben Walker <bwalke05@gmail.com>
2020-10-24 22:27:34 -0400

    Consolidate server logic

--- api/app.ts
+++
@@ -1,7 +0,0 @@
-import createExpress from "express";
-
-import { server } from "./server";
-
-export const app = createExpress();
-
-server.applyMiddleware({ app, path: "/" });
--- api/server/index.ts
+++ api/server/index.ts
@@ -1,6 +1,10 @@
 import { ApolloServer } from "apollo-server-express";
+import createExpress from "express";
 
 import { context } from "./context";
 import { schema } from "./schema";
 
-export const server = new ApolloServer({ context, schema });
+export const server = createExpress();
+const apollo = new ApolloServer({ context, schema });
+
+apollo.applyMiddleware({ app: server, path: "/" });
