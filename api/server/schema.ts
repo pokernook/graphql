ea938916d7e52ca4248837dc10388fabc26c6844
Ben Walker <bwalke05@gmail.com>
2020-11-11 18:32:08 -0500

    Adjust typegen outputs and inputs

--- api/server/schema.ts
+++ api/server/schema.ts
@@ -7,26 +7,32 @@ import * as types from "../graphql";
 export const schema = makeSchema({
   outputs: {
     schema: join(__dirname, "../api.graphql"),
-    typegen: join(
-      __dirname,
-      "../../node_modules/@types/nexus-typegen/index.d.ts"
-    ),
+    typegen: join(__dirname, "../../generated/@types/nexus-typegen/index.d.ts"),
   },
   plugins: [
     nexusPrisma({
       experimentalCRUD: true,
       paginationStrategy: "prisma",
+      inputs: {
+        prismaClient: join(__dirname, "../../generated/prisma/client"),
+      },
+      outputs: {
+        typegen: join(
+          __dirname,
+          "../../generated/@types/nexus-prisma-typegen/index.d.ts"
+        ),
+      },
     }),
   ],
   typegenAutoConfig: {
     sources: [
       {
         alias: "prisma",
-        source: require.resolve("@prisma/client/index.d.ts"),
+        source: join(__dirname, "../../generated/prisma/client/index.d.ts"),
       },
       {
         alias: "ContextModule",
-        source: require.resolve("./context"),
+        source: join(__dirname, "./context.ts"),
       },
     ],
     contextType: "ContextModule.Context",
