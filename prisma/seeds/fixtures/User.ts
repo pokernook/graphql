0e321087fc74ed8a9b5dd107089b0871420966ef
Ben Walker <bwalke05@gmail.com>
2020-11-14 19:14:02 -0500

    Add discriminator to seeds

--- prisma/seeds/fixtures/User.ts
+++ prisma/seeds/fixtures/User.ts
@@ -7,8 +7,20 @@ const passwordHash = // Translates to 'password'
   "$argon2id$v=19$m=4096,t=3,p=1$awIn+BOIcEF8KhDWVuUBPQ$f6VLaW4X0AmAnBMvdsnFssUjERdPAbpUC4UmrU2AoSk";
 
 const users: UserCreateInput[] = [
-  { id: "1", email: "test@email.com", username: "test", passwordHash },
-  { id: "2", email: "cowbell@email.com", username: "cowbell", passwordHash },
+  {
+    id: "1",
+    email: "test@email.com",
+    username: "test",
+    discriminator: 0,
+    passwordHash,
+  },
+  {
+    id: "2",
+    email: "cowbell@email.com",
+    username: "cowbell",
+    discriminator: 0,
+    passwordHash,
+  },
 ];
 
 export const seedUser = async (prisma: PrismaClient): Promise<void> => {
