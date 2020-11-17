3eb3150ae9f6635b38a345796353007504acde83
Ben Walker <bwalke05@gmail.com>
2020-11-16 20:18:40 -0500

    Use constants instead of magic numbers

--- api/graphql/User/helper.ts
+++ api/graphql/User/helper.ts
@@ -4,6 +4,9 @@ import { sign } from "jsonwebtoken";
 import { PrismaClient } from "../../../generated/prisma/client";
 import { config } from "../../config";
 
+const DISCRIMINATOR_CHECKS = 5;
+const MAX_DISCRIMINATOR = 10000;
+
 export const signToken = (userId: string): string =>
   sign({ userId }, config.appSecret);
 
@@ -11,7 +14,9 @@ export const uniqueDiscriminator = async (
   prisma: PrismaClient,
   username: string
 ): Promise<number | undefined> => {
-  const possibleDiscriminators = [...Array(5)].map(() => randomInt(10000));
+  const possibleDiscriminators = [...Array(DISCRIMINATOR_CHECKS)].map(() =>
+    randomInt(MAX_DISCRIMINATOR)
+  );
   for (const d of possibleDiscriminators) {
     const existingUser = await prisma.user.findOne({
       where: { UserTag: { username, discriminator: d } },
