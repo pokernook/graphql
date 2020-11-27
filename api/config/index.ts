7b272738c90a25c47d33f9a8699899af234193da
Ben Walker <bwalke05@gmail.com>
2020-11-26 20:05:47 -0500

    Localhost and client fixes (#97)
    * Switch to port 4000
    
    * Move prisma client creation outside of context function

--- api/config/index.ts
+++ api/config/index.ts
@@ -3,5 +3,5 @@ import env from "env-var";
 export const config = {
   appSecret: env.get("APP_SECRET").default("").asString(),
   env: env.get("NODE_ENV").default("development").asString(),
-  port: env.get("PORT").default(3000).asPortNumber(),
+  port: env.get("PORT").default(4000).asPortNumber(),
 };
--- api/nexus/context.ts
+++ api/nexus/context.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
+const prisma = new PrismaClient();
+
 export type Context = {
   prisma: PrismaClient;
 };
 
 export const context = (): Context => ({
-  prisma: new PrismaClient(),
+  prisma,
 });
