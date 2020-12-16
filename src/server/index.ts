4b928c13c87d4e783e86c576fda47536be85ea34
Ben Walker <bwalke05@gmail.com>
2020-12-15 22:49:04 -0500

    Configure sessions, fix session data type error

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,27 +1,12 @@
 import { ApolloServer } from "apollo-server-express";
 import createExpress from "express";
-import createSession from "express-session";
 import http from "http";
 
-import { config } from "../config";
 import { createContext } from "../context";
 import { schema } from "../schema";
+import { session } from "./session";
 
 const app = createExpress();
-const session = createSession({
-  // TODO: Switch away from in-memory session store
-  cookie: {
-    httpOnly: true,
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite: "lax",
-    secure: config.env === "production",
-  },
-  name: "user_session",
-  resave: false,
-  saveUninitialized: false,
-  secret: config.appSecret,
-  unset: "destroy",
-});
 app.use(session);
 export const server = http.createServer(app);
 
---
+++ src/server/session.ts
@@ -0,0 +1,23 @@
+import createSession from "express-session";
+
+import { config } from "../config";
+
+declare module "express-session" {
+  interface Session {
+    userId: string;
+  }
+}
+
+export const session = createSession({
+  cookie: {
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    sameSite: "lax",
+    secure: config.env === "production",
+  },
+  name: "user_session",
+  resave: false,
+  saveUninitialized: false,
+  secret: config.appSecret,
+  unset: "destroy",
+});
