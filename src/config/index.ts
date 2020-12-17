1f552111b0d83ebe8bfd2261a3216417ac497fdf
Ben Walker <bwalke05@gmail.com>
2020-12-16 20:22:22 -0500

    Add redis URL config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export const config = {
   appSecret: process.env.APP_SECRET || "",
   env: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT || "4000", 10),
+  redisUrl: process.env.REDIS_URL || "",
 };
