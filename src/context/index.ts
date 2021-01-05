28cbd5c5110661d16e34faaecd300c637ffc824d
Ben Walker <bwalke05@gmail.com>
2021-01-04 20:56:13 -0500

    Hydrate user on context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { Request } from "express";
 
 const prisma = new PrismaClient();
@@ -6,9 +6,11 @@ const prisma = new PrismaClient();
 export type Context = {
   prisma: PrismaClient;
   req: Request;
+  user: User | null;
 };
 
-export const createContext = (req: Request): Context => ({
-  prisma,
-  req,
-});
+export const createContext = async (req: Request): Promise<Context> => {
+  const id = req.session.userId || "";
+  const user = await prisma.user.findUnique({ where: { id } });
+  return { prisma, req, user };
+};
