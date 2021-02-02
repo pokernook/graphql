2cac0d0f2abf3661b1251abc5052b490fff0e661
Ben Walker <bwalke05@gmail.com>
2021-02-02 18:56:58 -0500

    Port context to fastify

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,16 +1,16 @@
 import { PrismaClient, User } from "@prisma/client";
-import { Request } from "express";
+import { FastifyRequest } from "fastify";
 
 const prisma = new PrismaClient();
 
 export type Context = {
   prisma: PrismaClient;
-  req: Request;
+  req: FastifyRequest;
   user: User | null;
 };
 
-export const createContext = async (req: Request): Promise<Context> => {
-  const id = req.session.userId || "";
+export const buildContext = async (req: FastifyRequest): Promise<Context> => {
+  const id = req.session.userId;
   const user = await prisma.user.findUnique({ where: { id } });
   return { prisma, req, user };
 };
