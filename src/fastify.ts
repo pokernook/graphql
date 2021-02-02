bdb37e07290297ca8d8d59f495038e4a57f58c6a
Ben Walker <bwalke05@gmail.com>
2021-02-02 09:40:49 -0500

    Register mercurius

--- src/fastify.ts
+++ src/fastify.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
+import mercurius from "mercurius";
 
 import { config } from "./config";
+import { schema } from "./schema";
 
 const buildApp = async () => {
   const app = Fastify();
+
+  await app.register(mercurius, { schema });
 
   return app;
 };
