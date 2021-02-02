cb2f6207fbce55f651b6a85c5344f22b48eaba2b
Ben Walker <bwalke05@gmail.com>
2021-02-02 18:44:29 -0500

    Setup fastify session

--- src/fastify.ts
+++ src/fastify.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import cookie from "fastify-cookie";
 import helmet from "fastify-helmet";
+import session from "fastify-session";
 import mercurius from "mercurius";
 
 import { config } from "./config";
@@ -11,6 +12,17 @@ const buildApp = async () => {
 
   await app.register(helmet);
   await app.register(cookie);
+  await app.register(session, {
+    cookie: {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      sameSite: true,
+      secure: config.env === "production",
+    },
+    cookieName: "user_session",
+    saveUninitialized: false,
+    secret: config.appSecret,
+  });
   await app.register(mercurius, {
     graphiql: config.env === "production" ? false : "playground",
     schema,
