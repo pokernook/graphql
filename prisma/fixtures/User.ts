8bcd9b3fb4be6ac45f6c4c88b7ddfe3f55ccd351
Ben Walker <bwalke05@gmail.com>
2021-02-11 18:47:02 -0500

    Remove unnecessary export

--- prisma/fixtures/User.ts
+++ prisma/fixtures/User.ts
@@ -3,7 +3,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const passwordHash = // Translates to 'password'
   "$argon2id$v=19$m=4096,t=3,p=1$awIn+BOIcEF8KhDWVuUBPQ$f6VLaW4X0AmAnBMvdsnFssUjERdPAbpUC4UmrU2AoSk";
 
-export const users: Prisma.UserCreateInput[] = [
+const users: Prisma.UserCreateInput[] = [
   {
     email: "test@email.com",
     username: "test",
