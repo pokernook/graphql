443e22747bb1deda36de9d61a7666e30f043cf6e
Ben Walker <bwalke05@gmail.com>
2021-03-02 22:10:09 -0500

    Add user status emoji scalar

---
+++ src/schema/types/Scalars.ts
@@ -0,0 +1,22 @@
+import emojiRegexRGI from "emoji-regex/RGI_Emoji";
+import { Kind } from "graphql";
+import { scalarType } from "nexus";
+
+export const UserStatusEmoji = scalarType({
+  name: "UserStatusEmoji",
+  asNexusMethod: "userStatusEmoji",
+  description: "An emoji summarizing the user's status",
+  parseValue: (value: string) => value,
+  serialize: (value: string) => value,
+  parseLiteral: (ast) => {
+    if (ast.kind !== Kind.STRING) {
+      throw new Error(`Expected string, got ${ast.kind}`);
+    }
+    const emojiRegex = emojiRegexRGI();
+    const firstMatch = emojiRegex.exec(ast.value)?.[0];
+    if (firstMatch !== ast.value) {
+      throw new Error("Invalid status emoji");
+    }
+    return ast.value;
+  },
+});
