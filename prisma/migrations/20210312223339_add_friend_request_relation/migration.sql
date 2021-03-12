88ced4d542ddd47e69e50c3f8a21902be73a649a
Ben Walker <bwalke05@gmail.com>
2021-03-12 17:37:41 -0500

    Add unique constraint to friend requests, also status

--- prisma/migrations/20210312223339_add_friend_request_relation/migration.sql
+++ prisma/migrations/20210312223339_add_friend_request_relation/migration.sql
@@ -1,12 +1,19 @@
+-- CreateEnum
+CREATE TYPE "FriendRequestStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
 -- CreateTable
 CREATE TABLE "FriendRequest" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "fromId" TEXT NOT NULL,
     "id" TEXT NOT NULL,
+    "status" "FriendRequestStatus" NOT NULL DEFAULT E'PENDING',
     "toId" TEXT NOT NULL,
 
     PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FriendRequest.fromId_toId_unique" ON "FriendRequest"("fromId", "toId");
 
 -- AddForeignKey
 ALTER TABLE "FriendRequest" ADD FOREIGN KEY ("fromId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
